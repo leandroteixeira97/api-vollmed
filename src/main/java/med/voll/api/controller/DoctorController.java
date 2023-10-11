403a5d7054fb088b8069741d7d037e28479ef767
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2023-10-10 22:34:18 -0300

    `fix`: Returning better server responses and updating `application.yaml` to hide JPA queries

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -6,6 +6,8 @@ import med.voll.api.doctor.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,8 +19,9 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid RegisterDoctorDTO registerDoctorDTO) {
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterDoctorDTO registerDoctorDTO) {
         doctorRepository.save(new Doctor(registerDoctorDTO));
+        return new ResponseEntity<>("The doctor was registered successfully", HttpStatus.OK);
     }
 
     @GetMapping
@@ -28,16 +31,19 @@ public class DoctorController {
 
     @PutMapping
     @Transactional
-    public void updateDoctor(@RequestBody @Valid UpdateDoctorDTO updateDoctorDTO) {
+    public ResponseEntity<String> updateDoctor(@RequestBody @Valid UpdateDoctorDTO updateDoctorDTO) {
         Doctor doctorOptional = doctorRepository.getReferenceById(updateDoctorDTO.id());
-
         doctorOptional.updateInformations(updateDoctorDTO);
+        return new ResponseEntity<>("The doctor of id " + updateDoctorDTO.id() +
+                " was updated successfully!", HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{doctorId}")
     @Transactional
-    public void deleteDoctor(@PathVariable Long doctorId) {
+    public ResponseEntity<String> deleteDoctor(@PathVariable Long doctorId) {
         Doctor doctor = doctorRepository.getReferenceById(doctorId);
         doctor.setExpired(true);
+        return new ResponseEntity<>("The doctor of id " + doctorId +
+                " was deleted successfully!", HttpStatus.OK);
     }
 }
